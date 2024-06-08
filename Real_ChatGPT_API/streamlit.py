fa847aa36f5590d5ddbb95a64763efabbdd05ddb
joohee6218 <jhkim@coinplug.com>
2024-06-08 14:47:18 +0900

    AI

---
+++ Real_ChatGPT_API/streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("나의 첫 번째 Streamlit App")
+
+st.header("Streamlit Header")
+st.subheader("Streamlit SubHeader")
+
+st.text("Normal Text")
+
+st.write("write() 함수를 사용하여 텍스트, 데이터 또는 플롯을 표시할 수 있다.")
