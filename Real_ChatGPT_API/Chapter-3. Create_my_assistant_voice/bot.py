40ca69882948c3a39e4f11a05bc6bf0d71b41b20
joohee6218 <jhkim@coinplug.com>
2024-06-13 14:55:50 +0900

    with

--- Real_ChatGPT_API/Chapter-3. Create_my_assistant_voice/bot.py
+++ Real_ChatGPT_API/Chapter-3. Create_my_assistant_voice/bot.py
@@ -3,7 +3,6 @@ import openai
 import base64
 import streamlit as st
 from audiorecorder import audiorecorder
-# 시간정보를 위한 패키지
 from datetime import datetime
 from gtts import gTTS
 
@@ -15,22 +14,23 @@ def STT(audio, apikey):
   
   # 음원 파일 열기
   audio_file = open(filename, 'rb')
+  
   # whisper 모델을 활용해 텍스트 얻기
-  client = openai.OpenAI(api_key = apikey)
-  respons = client.audio.transscriptions.create(model = "whisper-1", file = audio_file)
-  audio_file.close()
+  with open(filename, 'rb') as audio_file:
+    openai.api_key = apikey
+    response = openai.Audio.transscriptions.create(model = "whisper-1", file = audio_file)
+    
   # 파일 삭제
   os.remove(filename)
-  return respons.text
+  return response['text']
 
 def ask_gpt(prompt, model, apikey): # 질문 텍스트와 LLM모델을 입력으로 받고, ChatGPT의 답변을 반환하는 함수 askgpt() 선언
-  client = openai.OpenAI(api_key = apikey)
-  response = client.chat.completions.create(
+  openai.api_key = apikey
+  response = openai.ChatCompletion.create(
     model=model, # st.session_state["model"] 라디오 버튼으로 선택한 모델로 설정
     messages=prompt # 입력받은 프롬프트를 활용하여 질문 
   )
-  gptResponse = response.choices[0].message.content # GPT 모델을 통해 얻은 최종 답변 저장
-  return gptResponse
+  return response.choices[0].message['content'] # GPT 모델을 통해 얻은 최종 답변 저장
 
 def TTS(response):
   # gTTS를 활용하여 음성 파일 생성
@@ -54,26 +54,18 @@ def TTS(response):
 ##### main function #####
 def main():
   # 기본 설정
-  st.set_page_config(
-    page_title="Voice Assistant",
-    layout="wide"
-    )
-
+  st.set_page_config(page_title="Voice Assistant", layout="wide")
   st.header("Voice Assistant Program")
-
   st.markdown("---")
   
   ## 텍스트로 기본 설명
-  with st.expander("about voice assistant", expanded=True):
-    st.write(
-    """
+  with st.expander("About voice assistant", expanded=True):
+    st.write("""
     - Used streamlit
     - Used Whisper AI of OpenAI for STT(Speach-to-Text)
     - Used OpenAI Model
     - Used Google translate TTS(Text-to-Speach)
-    """
-    )
-    
+    """)
     st.markdown("")
 
   ## session state 초기화
@@ -84,7 +76,10 @@ def main():
     st.session_state["OPENAI_API"] = "" # 사용자가 입력한 OpenAI API를 저장하여 클라이언트 생성 시 사용
 
   if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistan. Respond to all input in 25 words and answer in korea."}] # GPT API에 입력(input)으로 전달할 프롬프트 양식을 저장. 이전 질문과 답변 모두 차례로 누적하여 저장.
+    st.session_state["messages"] = [
+      {"role": "system",
+       "content": "You are a thoughtful assistan. Respond to all input in 25 words and answer in korea."}
+    ] # GPT API에 입력(input)으로 전달할 프롬프트 양식을 저장. 이전 질문과 답변 모두 차례로 누적하여 저장.
 
   if "check_audio" not in st.session_state:
     st.session_state["check_reset"] = False # 사용자가 리셋 버튼을 클릭한 상태를 나타내는 플래그로 해당 플래그가 True일 경우 사용자의 입력을 받기 전에 프로그램이 동작하는 것을 방지.
@@ -104,7 +99,10 @@ def main():
       # 초기화 코드
       # 리셋 버튼을 누르면 기존 대화 내용을 모두 삭제하기 위해 st_session_state["chat"], st_session_state["message"], st_session_state["check_reset"]의 session_state를 초기화한다.
       st.session_state["chat"] = []
-      st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistant. Respond to all input in 25 words and answer in korea."}]
+      st.session_state["messages"] = [
+        {"role": "system", 
+         "content": "You are a thoughtful assistant. Respond to all input in 25 words and answer in korea."}
+      ]
       st.session_state["check_reset"] = True
 
   ## 기능 구현: 질문과 Chat-GPT의 답변을 아이폰의 문자 메세지 형태로 볼 수 있도록 구현한다.
@@ -124,9 +122,9 @@ def main():
       
       # 채팅을 시각화하기 위해 질문 내용 저장
       now = datetime.now().strftime("%H:%M")
-      st.session_state["chat"] = st.session_state["chat"]+[("user", now, question)]
+      st.session_state["chat"].append(("user", now, question))
       # GPT 모델에 넣을 프롬프트를 위해 질문 내용 저장
-      st.session_state["message"] = st.session_state["message"]+[{"role": "user", "content": question}]
+      st.session_state["messages"].append({"role": "user", "content": question})
 
   with col2:
     # 오른쪽 영역 작성
@@ -136,11 +134,11 @@ def main():
       response = ask_gpt(st.session_state["messages"], model, st.session_state["OPENAI_API"])
       
       # GPT 모델에 넣을 프롬프트를 위해 답변 내용 저장
-      st.session_state["messages"] = st.session_state ["messages"] + [{"role": "system", "content": "response"}]
+      st.session_state["messages"].append({"role": "user", "content": question})
       
       # 채팅 시각화를 위한 답변 내용 저장
       now = datetime.now().strftime("%H:%m")
-      st.session_state["chat"] = st.session_state["chat"] + [("bot", now, response)]
+      st.session_state["chat"].append(("bot", now, response))
       
       # 채팅 형식으로 시각화
       for sender, time, message in st.session_state["chat"]:
